e94b9c0479f0e50acb90febd8be1c86c00a87976
Anouar Belhassan <135267395+BelAnouar@users.noreply.github.com>
2024-10-02 09:55:30 +0100

    add layout pages

--- src/main/java/com/gestiondeprojet/servelets/TaskServelet.java
+++ src/main/java/com/gestiondeprojet/servelets/TaskServelet.java
@@ -1,5 +1,30 @@
 package com.gestiondeprojet.servelets;
 
-public class TaskServelet {
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public class TaskServelet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	 /**
+     * @see HttpServlet#HttpServlet()
+     */
+	public TaskServelet() {
+		super();
+	}
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+	protected void doGet(HttpServletRequest request,HttpServletResponse response) {
+		
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doPost(HttpServletRequest request,HttpServletResponse response) {
+		
+	}
+  
 }
